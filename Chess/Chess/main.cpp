0ef886caa1803a141c8e9b42fad5b8df78a84510
leandre70 <leandre.guertin@gmail.com>
2017-01-24 14:30:43 -0500

    Project Creation

---
+++ Chess/Chess/main.cpp
@@ -0,0 +1,11 @@
+/*
+* File name: main.cpp
+* File authors: Léandre Guertin and Boubacar Telly Bah
+* File created on: January 24 2017
+*/
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello world!" << std::endl;
+}
